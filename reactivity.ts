152319d63b159ad84eafb422c2f07cce29a31a63
ankh04 <ankh04@icloud.com>
2022-10-09 21:41:13 +0800

    用全局变量activeEffect替换硬编码

--- reactivity.ts
+++ reactivity.ts
@@ -1,11 +1,15 @@
 type Fn = () => any
+// 用来存储副作用函数的容器
 const bucket = new Set<Fn>()
 
+// 响应式数据
 const data = { text: "hello world!" }
 
 const obj = new Proxy(data, {
     get(target: { text: string }, p: string | symbol, receiver: any): any {
-        bucket.add(effect)
+        if (activeEffect) {
+            bucket.add(activeEffect)
+        }
         return target[p]
     },
     set(target: { text: string }, p: string | symbol, value: any, receiver: any): boolean {
@@ -17,12 +21,19 @@ const obj = new Proxy(data, {
     }
 })
 
-function effect() {
-    let node = document.querySelector("#app")
-    node.textContent = obj.text
+// 用全局变量存储注册的effect函数
+let activeEffect
+function effect(fn: Fn) {
+    activeEffect = fn
+    fn()
 }
 
-effect()
+effect(
+    () => {
+        let node = document.querySelector("#app")
+        node.textContent = obj.text
+    }
+)
 
 setTimeout(() => {
     obj.text = "hello again!"
