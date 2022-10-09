77a83ba3e191c54628649062b17dca6f0a9cc2e8
ankh04 <ankh04@icloud.com>
2022-10-09 22:01:05 +0800

    更改bucket结构，给每个对象的属性设置一个副作用函数容器

--- reactivity.js
+++ reactivity.js
@@ -1,24 +1,56 @@
-var bucket = new Set();
+// 用来存储副作用函数的容器
+// 使用weakMap作用容器
+var bucket = new WeakMap();
+// 响应式数据
 var data = { text: "hello world!" };
 var obj = new Proxy(data, {
     get: function (target, p, receiver) {
-        bucket.add(effect);
+        if (!activeEffect) {
+            // 如果没有activeEffect，直接返回
+            return;
+        }
+        // 根据target从容器中取出 depsMap
+        // depsMap 中根据对象属性索引副作用函数
+        var depsMap = bucket.get(target);
+        if (!depsMap) {
+            bucket.set(target, (depsMap = new Map()));
+        }
+        // 由 p 取出depsMap中保存的副作用函数集合
+        var deps = depsMap.get(p);
+        // 如果deps不存在，就新建
+        if (!deps) {
+            depsMap.set(p, (deps = new Set()));
+        }
+        // 添加activeEffect到桶里
+        deps.add(activeEffect);
+        // 返回p索引的值
         return target[p];
     },
     set: function (target, p, value, receiver) {
         target[p] = value;
-        bucket.forEach(function (fn) {
+        // 取出depsMap
+        var depsMap = bucket.get(target);
+        if (!depsMap)
+            return;
+        // 根据key取出相应的副作用函数们
+        var effects = depsMap.get(p);
+        // 短路
+        effects && effects.forEach(function (fn) {
             fn();
         });
         return true;
     }
 });
-function effect() {
+// 用全局变量存储注册的effect函数
+var activeEffect;
+function effect(fn) {
+    activeEffect = fn;
+    fn();
+}
+effect(function () {
     var node = document.querySelector("#app");
     node.textContent = obj.text;
-    // window.document.body.innerHTML = obj.text
-}
-effect();
+});
 setTimeout(function () {
     obj.text = "hello again!";
 }, 1000);
--- reactivity.ts
+++ reactivity.ts
@@ -1,20 +1,46 @@
 type Fn = () => any
+type DepsMap = Map<string | symbol, Set<Fn>>
 // 用来存储副作用函数的容器
-const bucket = new Set<Fn>()
+// 使用weakMap作用容器
+// 这里使用weakMap的原因是，在被代理对象引用失效后，不持续引用， 方便垃圾回收
+const bucket = new WeakMap<object, DepsMap>()
 
 // 响应式数据
 const data = { text: "hello world!" }
 
 const obj = new Proxy(data, {
     get(target: { text: string }, p: string | symbol, receiver: any): any {
-        if (activeEffect) {
-            bucket.add(activeEffect)
+        if (!activeEffect) {
+            // 如果没有activeEffect，直接返回
+            return
         }
+        // 根据target从容器中取出 depsMap
+        // depsMap 中根据对象属性索引副作用函数
+        let depsMap = bucket.get(target)
+        if (!depsMap) {
+            bucket.set(target, (depsMap = new Map()))
+        }
+        // 由 p 取出depsMap中保存的副作用函数集合
+        let deps = depsMap.get(p)
+        // 如果deps不存在，就新建
+        if (!deps) {
+            depsMap.set(p, (deps = new Set()))
+        }
+        // 添加activeEffect到桶里
+        deps.add(activeEffect)
+
+        // 返回p索引的值
         return target[p]
     },
     set(target: { text: string }, p: string | symbol, value: any, receiver: any): boolean {
         target[p] = value
-        bucket.forEach(fn => {
+        // 取出depsMap
+        const depsMap = bucket.get(target)
+        if (!depsMap) return
+        // 根据key取出相应的副作用函数们
+        const effects = depsMap.get(p)
+        // 短路
+        effects && effects.forEach(fn => {
             fn();
         })
         return true
